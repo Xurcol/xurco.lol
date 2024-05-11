a0d6d83e340cddf90b233eb799dc7e3de6d5656e
Xurcol <104295572+Xurcol@users.noreply.github.com>
2024-05-11 14:40:39 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+console.log("explicit#0001");
+
+let counter = 0; let Title = 'xurco.lol'; let direction = true;
+aniTitle = setInterval(function() {
+  if (counter == Title.length) direction = false;
+  if (counter == false) direction = true;
+  counter = (direction == true) ? ++counter : --counter;
+  newtitle = (counter == 0) ? "" : Title.slice(0, counter);
+  document.title = '' + newtitle;
+}, 400)
+
+window.oncontextmenu = function() {
+  return false;
+}
+
+function audioPlay() {
+  var audio = document.getElementById("audio");
+  audio.volume = 0.3;
+  audio.play()
+}
+
+function videoPlay() {
+  var video = document.getElementById("video");
+  video.play()
+}
